a0fbcb24036e3b68411e4a4572d1a019f374aee2
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-14 15:08:06 -0400

    Mongoose para conectar com mongodb atlas

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const moogoose = require('mongoose');
 const app = express();
 
-
-
+moogoose.connect('mongodb+srv://omnistack:omnistack@devradarcluster-hcvki.mongodb.net/week10?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
 app.listen(3333);
