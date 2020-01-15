02ccfa9d5cb97ea0741e859f771e092c54ca4efb
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-15 00:51:04 -0400

    Corrigindo vulnerabilidade de dados sensiveis expostos no versionamento: Chegaram tarde :D

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const moogoose = require('mongoose');
-
+const {usuario, senha, banco} = require('./config/config.json')
 
 const routes = require('./routes');
 
 
 const app = express();
 
-moogoose.connect('mongodb+srv://omnistack:omnistack@devradarcluster-hcvki.mongodb.net/week10?retryWrites=true&w=majority',
+moogoose.connect(`mongodb+srv://${usuario}:${senha}@devradarcluster-hcvki.mongodb.net/${banco}?retryWrites=true&w=majority`,
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
