76aa7976fe7da165e6c2c400971d6b472de31e02
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-15 15:22:21 -0400

    side estilizado

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 
 import './global.css';
+import './Sidebar.css'
+import './App.css';
 
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
 
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
       <main>
         
