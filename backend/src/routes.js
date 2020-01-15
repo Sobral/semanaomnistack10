50faf64687d6e30f900cb04d96ec25ace1d574da
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-14 23:37:47 -0400

    Criando controller de um dev, e minimizando o codigo no router

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,36 +1,11 @@
 const {Router} = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
 
 
-routes.post('/devs', async (request, response) => {
-    const {github_username, techs, latitude, longitude} = request.body;
-
-    const github_api_response = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const {name = login, avatar_url, bio} = github_api_response.data;
-
-    techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location: location
-    });
-
-    return response.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 
 module.exports = routes;
