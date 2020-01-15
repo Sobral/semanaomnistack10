0f6e23110b754bba15b296e3f0c44d124ccc96c2
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-14 22:40:44 -0400

    POST /devs implementada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,29 @@
 const {Router} = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 
 
-routes.get('/users', (request, response) => {
+routes.post('/devs', async (request, response) => {
+    const {github_username, techs} = request.body;
 
+    const github_api_response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const {name = login, avatar_url, bio} = github_api_response.data;
+
+    techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    });
+
+    return response.json(dev);
 });
 
 
