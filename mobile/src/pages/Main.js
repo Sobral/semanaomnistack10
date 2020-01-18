e821570de08fc09689ebd11467622d88a8bf80b1
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-18 16:04:28 -0400

    Posicionando avatar do devel no mapa

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native'
+import MapView, {Marker} from 'react-native-maps';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 
 function Main() {
@@ -18,8 +18,8 @@ function Main() {
                 setCurrentRegion({
                     latitude,
                     longitude,
-                    latitudeDelta: 0.02,
-                    longitudeDelta: 0.02,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
                 });
             }
         }
@@ -31,12 +31,25 @@ function Main() {
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map}/>
+    return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude:-3.0780363,longitude:-60.0606397} }>
+        <Image source={{uri:'https://avatars1.githubusercontent.com/u/1907108?v=4'}} style={styles.avatar}/>
+
+        </Marker>
+    </MapView>);
 }
 
 const styles = StyleSheet.create({
     map:{
         flex:1
+    },
+    avatar:{
+        width:54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
     }
 });
 
