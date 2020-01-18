e594541c5ba3b877614b57dae95edbe9a699956f
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-18 16:17:50 -0400

    Ao clicar no avatar do Dev, seu nome, sua bio e suas techs aparecem num balao de texto

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
-import { StyleSheet, Image } from 'react-native'
-import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, {Marker, Callout} from 'react-native-maps';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 
 function Main() {
@@ -35,7 +35,13 @@ function Main() {
     <MapView initialRegion={currentRegion} style={styles.map}>
         <Marker coordinate={{ latitude:-3.0780363,longitude:-60.0606397} }>
         <Image source={{uri:'https://avatars1.githubusercontent.com/u/1907108?v=4'}} style={styles.avatar}/>
-
+        <Callout>
+            <View style={styles.callout}>
+                <Text style={styles.DevName}>Luciano Sobral</Text>
+                <Text style={styles.DevBio}>Manda o job que eu mato a parada</Text>
+                <Text style={styles.DevTechs}>Nodejs, React, ReactNative</Text>
+            </View>
+        </Callout>
         </Marker>
     </MapView>);
 }
@@ -50,6 +56,18 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#fff'
+    },
+    callout:{width:260},
+    DevName:{
+        fontWeight: "bold",
+        fontSize: 16
+    },
+    DevBio:{
+        color:'#666',
+        marginTop:5
+    },
+    DevTechs:{
+        marginTop:5
     }
 });
 
