cac6cc9b258461acf5dd590c00d0dbf8419bde23
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-18 18:02:49 -0400

    Adicionando campo de busca  e botao de geolocalizacao

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, {Marker, Callout} from 'react-native-maps';
 import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+import {MaterialIcons} from '@expo/vector-icons';
 
 function Main({navigation}) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -32,21 +33,38 @@ function Main({navigation}) {
     }
 
     return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude:-3.0780363,longitude:-60.0606397} }>
-        <Image source={{uri:'https://avatars1.githubusercontent.com/u/1907108?v=4'}} style={styles.avatar}/>
-        <Callout onPress={()=>{
-                navigation.navigate('Profile', {github_username:'Sobral'});
-            }
-        }>
-            <View style={styles.callout}>
-                <Text style={styles.DevName}>Luciano Sobral</Text>
-                <Text style={styles.DevBio}>Manda o job que eu mato a parada</Text>
-                <Text style={styles.DevTechs}>Nodejs, React, ReactNative</Text>
-            </View>
-        </Callout>
-        </Marker>
-    </MapView>);
+    <>
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude:-3.0780363,longitude:-60.0606397} }>
+            <Image source={{uri:'https://avatars1.githubusercontent.com/u/1907108?v=4'}} style={styles.avatar}/>
+            <Callout onPress={()=>{
+                    navigation.navigate('Profile', {github_username:'Sobral'});
+                }
+            }>
+                <View style={styles.callout}>
+                    <Text style={styles.DevName}>Luciano Sobral</Text>
+                    <Text style={styles.DevBio}>Manda o job que eu mato a parada</Text>
+                    <Text style={styles.DevTechs}>Nodejs, React, ReactNative</Text>
+                </View>
+            </Callout>
+            </Marker>
+        </MapView>
+        
+        <View style={styles.searchForm}>
+        <TextInput style={styles.searchInput}
+            placeholder="Buscar Devs por techs"
+            placeholderTextColor="#999"
+            autoCapitalize="words"
+            autoCorrect={false}
+        />
+
+        <TouchableOpacity style={styles.loadButtom} onPress={()=>{return}}>
+            <MaterialIcons name="my-location" size={20} color="#FFF"></MaterialIcons>
+        </TouchableOpacity>
+        </View>
+    </>
+    );
+    
 }
 
 const styles = StyleSheet.create({
@@ -60,7 +78,8 @@ const styles = StyleSheet.create({
         borderWidth: 4,
         borderColor: '#fff'
     },
-    callout:{width:260},
+    callout:{
+        width:260},
     DevName:{
         fontWeight: "bold",
         fontSize: 16
@@ -71,7 +90,42 @@ const styles = StyleSheet.create({
     },
     DevTechs:{
         marginTop:5
+    },
+    searchInput:{
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset:{
+            height: 4,
+            width: 4
+        },
+        elevation: 3
+    },
+    searchForm:{
+        position:'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    loadButtom:{
+        width:50,
+        height:50,
+        backgroundColor:'#8e4dff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft:15,
+        borderRadius:25
     }
+
 });
 
 export default Main;
