75d5eaf6c1b1e77079dca631f43db370187cca00
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-18 11:11:06 -0400

    Localizando regiao atual do usuario

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,43 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { StyleSheet } from 'react-native'
 import MapView from 'react-native-maps';
+import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 
 function Main() {
-    return <MapView style={{flex:1}}/>
+    const [currentRegion, setCurrentRegion] = useState();
+
+    useEffect(() => {
+        async function loadInicialPosition(){
+            const {granted } = await requestPermissionsAsync();
+            if(granted){
+                const {coords} = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const {latitude, longitude} = coords;
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.02,
+                    longitudeDelta: 0.02,
+                });
+            }
+        }
+
+        loadInicialPosition();
+    },[]);
+
+    if(!currentRegion){
+        return null;
+    }
+
+    return <MapView currentRegion={currentRegion} style={styles.map}/>
 }
+
+const styles = StyleSheet.create({
+    map:{
+        flex:1
+    }
+});
 
 export default Main;
