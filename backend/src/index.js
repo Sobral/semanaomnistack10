88e95f3fe520e27955d50b01481fc07fba5dc4c9
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-19 14:16:04 -0400

    Implementação base de real time com socketio

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const moogoose = require('mongoose');
-const {usuario, senha, banco} = require('./config/config.json')
+const http = require('http');
+const {usuario, senha, banco} = require('./config/config.json');
+const {setupWebSocket} = require('./websocket');
 const cors = require('cors');
 const routes = require('./routes');
 
 
 const app = express();
+const server = http.Server(app);
+setupWebSocket(server);
 
 moogoose.connect(`mongodb+srv://${usuario}:${senha}@devradarcluster-hcvki.mongodb.net/${banco}?retryWrites=true&w=majority`,
 {
@@ -19,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
