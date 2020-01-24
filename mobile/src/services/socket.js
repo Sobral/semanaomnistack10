d7d0e5affd37c176bb8fcd28df1e9b6a7c9ffa87
Luciano Sobral <sobral.luciano@gmail.com>
2020-01-24 14:00:18 -0400

    Implementando subscribeToNewDevs

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.1.123:3333', {
     autoConnect: false
 });
 
+function subscribeToNewDevs(subscribeFunction){
+    socket.on('new-dev', subscribeFunction );
+}
+
 function connect(latitude, longitude, techs){
     console.log(latitude, longitude, techs);
 
